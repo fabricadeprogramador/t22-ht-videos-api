6059b06007a57401f3fbff00ad445689c99b1f95
kennedylima <kennedyoliveiralima@gmail.com>
2019-05-25 12:14:45 -0400

    Mundando atributo de videos para filmes

--- models/categoria-model.js
+++ models/categoria-model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const CategoriaSchema = new mongoose.Schema({
   nome: String,
-  videos: Array
+  filmes: Array
 });
 
 export default mongoose.model('Categoria', CategoriaSchema);
