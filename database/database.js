1f29e7d01efea90f597550233ad8bace3a7f2c70
kennedylima <kennedyoliveiralima@gmail.com>
2019-07-20 10:23:47 -0400

    Implementando as configuracoes de acesso ao banco em producao

--- database/database.js
+++ database/database.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 mongoose.Promise = Promise;
+const MONGODB_URI_DEV = 'mongodb://localhost/HTVideos';
+const MONGODB_URI_PROD = 'mongodb+srv://admin:admin@ht-videos-vyvuc.mongodb.net/test?retryWrites=true&w=majority';
 
-const mongodbUrl = 'mongodb://localhost/HTVideos';
+
+const mongodbUrl = process.env.NODE_ENV === 'production' ? MONGODB_URI_PROD : MONGODB_URI_DEV;
 const connect = () => mongoose.connect(mongodbUrl, { useNewUrlParser: true });
 
 export default { connect };
